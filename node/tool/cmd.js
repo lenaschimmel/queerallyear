aed6fc301faa15e6b3fc53dc75caa4259838dbbf
Lena Schimmel <lena@serioese.gmbh>
2019-06-17 22:33:16 +0200

    Fix cmd tool's svg outpzt

--- node/tool/cmd.js
+++ node/tool/cmd.js
@@ -12,7 +12,9 @@ function processSvgFile() {
         var gradients = doc.getElementsByTagName("defs").item(0);
         recolor.prepareGradients(gradients);
         recolor.changeGradients(gradients, flags.pan);
-        fs.writeFileSync("../output.svg", dom.serialize());
+        var start = '<!--?xml version="1.0" encoding="UTF-8" standalone="no"?--><html><head></head><body>';
+        var end = '</body></html>';
+        fs.writeFileSync("../output.svg", dom.serialize().replace(start, "").replace(end, ""));
     });
 }
 
