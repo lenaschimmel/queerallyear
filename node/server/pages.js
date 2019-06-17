1ae9cf96d2a691209cc57bf5d50b4832f29e9369
Lena Schimmel <lena@serioese.gmbh>
2019-06-17 22:32:56 +0200

    Modularize website as semi-dynamic

---
+++ node/server/pages.js
@@ -0,0 +1,14 @@
+exports.pages = [
+    {
+        "name" : "index",
+        "title" : "Startseite",
+        "cardtitle" : "Gay for one Day? Queer all Year!",
+        "description" : "Wir sind nicht nur gay, sondern auch lesbisch, bi, pan, asexuell, trans, polyamor, oder auch einfach: queer. Wir haben nicht immer Grund, fröhlich zu sein. Wir kämpfen nach wie vor um Sichtbarkeit, Anerkennung, Aufklärung und körperliche Selbstbestimmung.",
+    },
+    {
+        "name" : "impressum",
+        "title" : "Impressum / Datenschutz",
+        "cardtitle" : "Wer steckt hinter 'Queer all Year'?",
+        "description" : "Lies unser Impressum und unsere Datenschutzerklärung.",
+    },
+];
--- node/server/server.js
+++ node/server/server.js
@@ -2,16 +2,62 @@
 var express = require('express');
 var fs = require('fs');
 var app = express();
+var { pages } = require('./pages.js');
 
+var logo = fs.readFileSync("../../web/img/logo.svg").toString();
+logo = logo.substring(logo.indexOf("?>") + 2).replace('id="Ebene_1"', 'id="mainlogo"');
 
-var index = fs.readFileSync("../../web/index.html").toString();
-var logo  = fs.readFileSync("../../web/img/logo.svg").toString();
-logo = logo.substring(logo.indexOf("?>")+2).replace('id="Ebene_1"', 'id="mainlogo"');
-var output = index.replace('<img id="mainlogo" src="img/logo.svg" alt="Queer all year">', logo);
+var fragments = {};
 
-app.get('/', function (req, res) {
-  res.send(output);
-});
+function getNavi(activeName) {
+  var navi = "";
+  for (const key in pages) {
+    if (pages.hasOwnProperty(key)) {
+      const page = pages[key];
+      if (page.name == activeName)
+        navi += '<li><a href="/' + page.name + '.html" class="active">' + page.title + '</a></li>';
+      else
+        navi += '<li><a href="/' + page.name + '.html">' + page.title + '</a></li>';
+    }
+  }
+  return navi;
+}
+
+function getPageOutput(page) {
+  const content = fragment(page.name);
+  const output = 
+      fragment('head')
+        .replace("$NAVI", getNavi(page.name))
+        .replace("$TITLE", "Queer All Year - " + page.title) 
+        .replace("$CARDTITLE", page.cardtitle)
+        .replace("$DESCRIPTION", page.description)
+    + content 
+        .replace("$MAINLOGO", logo)
+    + fragment('foot');
+  return output;
+}
+
+function fragment(name) {
+  //if (!fragments[name]) {
+    fragments[name] = fs.readFileSync("../../web/contents/" + name + ".fragment").toString();
+  //}
+  return fragments[name];
+}
+
+for (const key in pages) {
+  if (pages.hasOwnProperty(key)) {
+    const page = pages[key];
+    const output = getPageOutput(page)
+    app.get('/' + page.name + ".html", function (req, res) {
+      res.send(output);
+    });
+    if(page.name == "index") {
+      app.get('/', function (req, res) {
+        res.send(output);
+      });
+    }
+  }
+}
 
 app.use(express.static('../../web/'));
 
